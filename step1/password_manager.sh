01d6fa9238cf98c270fc99303591df8181d875c6
HUSKcode <y.hirata1032@gmail.com>
2024-12-02 21:43:05 +0900

    done

---
+++ step1/password_manager.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PAAWORD_FILE="$PWD/password_manager.txt"
+
+echo "パスワードマネージャーへようこそ！"
+
+read -p "サービス名を入力してください：" service
+read -p "ユーザー名を入力してください：" username
+read -s -p "パスワードを入力してください：" password
+echo
+
+#入力情報を保存
+echo "$service:$username:$password" >> "PASSWORD_FILE"
+
+echo "Thank you!"
